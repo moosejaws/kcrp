bcf6ff8b2a44861fbd3588b85deeed89b307bd27
Reina R <rrodriguez.ut@gmail.com>
2024-04-04 16:01:53 -0700

    update

--- src/components/About.js
+++ src/components/About.js
@@ -87,7 +87,6 @@ export default function About() {
 
                 <div>
                
-                <Team />
 
 
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -36,7 +36,6 @@ export default function Navigation() {
           </div>
         </div>
 
-        
           
           </div>
       
