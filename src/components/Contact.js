b838723c0254d7e63b38d6218c04bfef73510e06
Reina R <rrodriguez.ut@gmail.com>
2024-04-04 21:59:26 -0700

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,13 +15,14 @@ export default function Contact () {
 
 
                   <div class='w-full    align-center justify-center vertical lg:gap-2 mt-12'>
-                      <div className='grid grid-cols-0 mt-0' >      <p class='text-white text-center font-semibold text-md lg:text-xl text-center m-auto '>If you or your family were affected by home or business loss in the Central District, Seattle or other areas of King County, we would like to document your story.</p>
+                      <div className='grid grid-cols-0 mt-0' >      <p class='text-white text-center font-regular
+                       text-sm lg:text-md text-center m-auto '>If you or your family were affected by home or business loss in the Central District, Seattle or other areas of King County, we would like to document your story.</p>
                 </div>
                 <div className="grid grid-cols-0 lg:grid-cols-3 mt-10 flex items-center lg:justify-left  gap-x-6 gap-y-6 justify-center">
                   <a
                     href="https://forms.gle/DpUVMJh9qgK9pLBr9"
                     target='_blank'
-                    className="rounded-sm hover:bg-[#FCDD09]  px-3.5 py-3.5 lg:py-4  border-2 border-inside border-[#FCDD09]  text-center lg:text-xs text-sm font-semibold text-white shadow-sm   hover:text-zinc-950 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-sm hover:bg-[#FCDD09]  px-3.5 py-3.5 lg:py-4  border-2 border-inside border-[#FCDD09]  text-center lg:text-xs text-xs font-semibold text-white shadow-sm   hover:text-zinc-950 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                   INTEREST FORM     
                   </a>
